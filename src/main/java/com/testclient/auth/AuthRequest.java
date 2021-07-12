04cfacd6d4256ff20c6e0a806ff131be53497888
KirillAstafev <gamerstyle.ya.ru@gmail.com>
2021-07-12 20:24:54 +0300

    Добавление класса Main, формата отправки данных для аутентификации студента (AuthRequest) и файла pom.xml

---
+++ src/main/java/com/testclient/auth/AuthRequest.java
@@ -0,0 +1,11 @@
+package com.testclient.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthRequest {
+    private String login;
+    private String password;
+}
